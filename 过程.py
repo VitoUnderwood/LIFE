5aa2dcd3ead34f33f52dcc0feb19124f5169a392
晓雅 <73062394+gxy734374658@users.noreply.github.com>
2020-10-21 22:33:25 +0800

    Update 过程.py

--- 过程.py
+++ 过程.py
@@ -1,7 +1,1 @@
-scores = {"Chinese":80,"English":100}
-Math_score = {"Math":99}
-scores.update(Math_score)
-print(scores)
-History_score = {"History":88}
-scores.update(History_score)
-print(scores)
+print(min(88,99,100))
