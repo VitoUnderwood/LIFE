0639d32f094a126de327b87c5ad8968f3fb02fc0
晓雅 <73062394+gxy734374658@users.noreply.github.com>
2020-10-22 22:26:57 +0800

    Update 过程.py

--- 过程.py
+++ 过程.py
@@ -1,1 +1,25 @@
-print(min(88,99,100))
+#同时给多个变量赋值
+a,b,c = 1,2,3
+print(a)
+print(b)
+print(c)
+#同时给多个变量赋同一个值
+x = y = z = 2
+print(x)
+print(y)
+print(z)
+#给一个变量赋多个值
+values = 1,2,3
+print(values)
+#增量赋值
+num = 5
+print(num)
+num += 1    #num = num + 1
+print(num)
+num *= 2    #num = num * 2
+print(num)
+name = "A"
+name += "B" #name = name + "B"
+print(name)
+name *= 2   #name = name * 2
+print(name)
