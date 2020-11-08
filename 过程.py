bfffd24126709fe0edae45e0c361481bfeb8b728
晓雅 <73062394+gxy734374658@users.noreply.github.com>
2020-11-08 22:11:16 +0800

    Update 过程.py

--- 过程.py
+++ 过程.py
@@ -1,10 +1,6 @@
-# not
-age = int(input("年龄："))
-if not (age < 18):
-    grade = int(input("年级："))
-    if grade >= 12:
-        print("OK")
-    else:
-        print("不行！")
-else:
-    print("年龄不满18")
+x = [1,3,2,5,4]
+for i in range(len(x)-1):
+    for j in range(i+1,len(x)):
+        if x[i] > x[j]:
+            x[i],x[j] = x[j],x[i]
+print(x)
